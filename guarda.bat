00002924c54c94efc0e668b8fd63f2c59703058a
dandino <dandinoc@ransa.net>
2023-09-07 16:13:56 -0600

    1_2023_09_07_15_58_03

--- guarda.bat
+++ guarda.bat
@@ -8,15 +8,38 @@ set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%"
 set "Min=%dt:~10,2%"
 set "Seg=%dt:~12,2%"
- 
 set "dia_hora=%AAAA%_%MM%_%DD%_%HH%_%Min%_%Seg%"
 
+echo 1. Cargar Actualizacion
+echo 2. Actualizar Rama de orige
+echo Q. Salir
+echo
+set /P tarea=Ingrese Valor de tarea:
+SWITCH tarea
+CASE 1 
+
+set /P COMENTARIO=Ingrese un comentario:
 git add .
-
-::git pull origin Dandino
-
-git commit -m %dia_hora%
-
+git commit -m %COMENTARIO%_%dia_hora%
 git push -u origin Dandino
+
+CASE 2
+
+git pull origin Dandino
+
+CASA Q or q
+echo adios
+ENDSWITCH
+::CASEALL
+
+::[DEFAULT]
+
+::set /P COMENTARIO=Ingrese un comentario:
+::git add .
+::git commit -m %dia_hora%
+::git commit -m %COMENTARIO%_%dia_hora%
+::git push -u origin Dandino
+
+
 
 pause
