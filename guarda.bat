a0b8db9ff50b66c343a9b5d690c1a090b030a527
dandino <dandinoc@ransa.net>
2023-09-08 09:44:18 -0600

    actualizacion_2023_09_08_09_44_12

--- guarda.bat
+++ guarda.bat
@@ -1,4 +1,6 @@
 @echo off
+TITLE Bienvenid@ %USERNAME% a @lm_blog menu
+MODE con:cols=80 lines=40
 
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "AA=%dt:~2,2%"
@@ -10,25 +12,29 @@ set "Min=%dt:~10,2%"
 set "Seg=%dt:~12,2%"
 set "dia_hora=%AAAA%_%MM%_%DD%_%HH%_%Min%_%Seg%"
 
-:Menu
+:inicio
 cls
 echo 1. Cargar Actualizacion
 echo 2. Actualizar Rama de orige
 echo Q. Salir
 set /P tarea=Ingrese Valor de tarea:
+if "%tarea%"=="0" goto op0
+if "%tarea%"=="1" goto op1
+if "%tarea%"=="2" goto op2
+if "%tarea%"=="q" goto salir
 
-:inicio
-cls
-	if "%tarea%"=="0" goto inicio
-	if "%tarea%"=="1" goto op1
-	if "%tarea%"=="2" goto op2
-	if "%tarea%"=="3" goto op3
-	if "%tarea%"=="4" goto op4
-	if "%tarea%"=="5" goto op5
-	if "%tarea%"=="6" goto salir
+::inicio
+::cls
+::	if "%tarea%"=="0" goto inicio
+::	if "%tarea%"=="1" goto op1
+::	if "%tarea%"=="2" goto op2
+::	if "%tarea%"=="3" goto op3
+::	if "%tarea%"=="4" goto op4
+::	if "%tarea%"=="5" goto op5
+::	if "%tarea%"=="6" goto salir
 
 ::Mensaje de error, validación cuando se selecciona una opción fuera de rango
-echo. El numero "%var%" no es una opcion valida, por favor intente de nuevo.
+echo. El numero "%tarea%" no es una opcion valida, por favor intente de nuevo.
 echo.
 pause
 echo.
