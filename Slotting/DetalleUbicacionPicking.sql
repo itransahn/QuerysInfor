283b1962fb5422fd6befe66c980f323162f0f13a
Douglas Alonzo Andino Castejo <DAndinoC@ransa.net>
2023-09-30 09:23:26 -0600

    Cambio detallede ubicaciones

--- Slotting/DetalleUbicacionPicking.sql
+++ Slotting/DetalleUbicacionPicking.sql
@@ -1,25 +1,21 @@
 SELECT 
 Codigo,
 ubicacion,
-/* Proveedor */
 (SELECT 
 top 1 isnull(suppliername, '')
 FROM 
 WMWHSE51.RECEIPTDETAIL R 
 WHERE R.sku = Codigo)as Proveedor,
-/* Entrada */
 (SELECT 
 MAX(r.datereceived)
 FROM 
 WMWHSE51.RECEIPTDETAIL R 
 WHERE R.sku = Codigo)as Entrada,
-/* Salida */
 (SELECT 
 MAX(r.pickeddate)
 FROM 
 WMWHSE51.PICKDETAIL R 
 WHERE R.sku = Codigo)as Salida,
-/* Stock */
 (
 SELECT 
 SUM(lot.qty/ NULLIF(p1.CASECNT,0))
@@ -36,24 +32,23 @@ ISNULL(EAN13 , '' ) as  EAN13
 FROM
 (
 SELECT 
-lot.sku as Codigo,
-s.descr  as Descripcion,
-ISNULL(L.putawayzone,'') as Zona,
-ISNULL(ALS.altsku   , '') as EAN13,
-ISNULL(locationtype , '') as Tipo_Ubicacion,
+ISNULL(lot.sku, 'NO ASIGNADO') as Codigo,
+ISNULL(s.descr, 'NO ASIGNADO') as Descripcion,
+ISNULL(L.putawayzone,'NO ASIGNADO') as Zona,
+ISNULL(ALS.altsku   , 'NO ASIGNADO') as EAN13,
+ISNULL(locationtype , 'NO ASIGNADO') as Tipo_Ubicacion,
 (
 CASE lot.status
 WHEN 'OK' then 'NO'
 ELSE 'SI'
 END
 )as Bloqueada,
-lot.loc as ubicacion
-FROM
-WMWHSE51.LOTXLOCXID lot 
+L.loc as ubicacion
+FROM WMWHSE51.LOTXLOCXID lot 
 LEFT JOIN WMWHSE51.SKU S  ON lot.sku = S.sku
 LEFT JOIN WMWHSE51.ALTSKU ALS ON lot.sku = ALS.sku AND ALS.TYPE like '%EAN13%'
-LEFT JOIN WMWHSE51.LOC L  ON lot.loc = l.loc
-WHERE lot.qty > 0 AND l.loclevel = 1 --AND lot.sku like '%BV440048%'
+RIGHT JOIN WMWHSE51.LOC L  ON lot.loc = l.loc
+WHERE  l.loclevel = 1 --AND lot.sku like '%BV440048%'
 )T0
 -- WMWHSE51.RECEIPTDETAIL R 
 -- WHERE R.sku like '%BV440048%'
@@ -61,3 +56,4 @@ WHERE lot.qty > 0 AND l.loclevel = 1 --AND lot.sku like '%BV440048%'
 
 
 
+SELECT COUNT(*),'' FROM LOC WHERE loclevel = 1 /*6996*/
