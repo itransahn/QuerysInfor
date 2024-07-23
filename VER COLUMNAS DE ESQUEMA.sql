5094c208cd5a3db4e137122405ed3dcd31218b6b
dandinoc <dandinoc@ransa.net>
2024-07-23 13:32:59 -0600

    REPORTES DE EXCELENCIA

--- VER COLUMNAS DE ESQUEMA.sql
+++ VER COLUMNAS DE ESQUEMA.sql
@@ -1,7 +1,9 @@
---pruebas de carga
+--pruebTHEN de carga
 SELECT table_name,
        column_name,
        data_type
 FROM information_schema.columns
 WHERE table_schema = 'wmwhse51'
-    AND column_name = 'TASKTYPE';
+    AND column_name = 'EQUIPMENTID'
+
+
